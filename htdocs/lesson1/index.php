a9da626e6ea930981d28199d5e9efddfda353bbd
lorisscanollii <153609334+lorisscanollii@users.noreply.github.com>
2025-03-04 20:46:31 +0100

    day1

---
+++ htdocs/lesson1/index.php
@@ -0,0 +1,18 @@
+<?php
+    $school = "HAHAHAHAHAHAHAHAHAH";
+    $nr1 = 6;
+    $nr2 = 8;
+
+    echo $nr1 + $nr2;
+    echo "<br>";
+
+    echo $nr1 * $nr2;
+    echo "<br>";
+
+    echo $nr1 % $nr2;
+    echo "<br>";
+
+    echo $nr1 / $nr2;
+    echo "<br>";
+    echo "This os another page of php";
+?>
